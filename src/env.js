2e7a09f6ad85573071752159b57606d19e2132ab
Guillermo de la Puente <g.puente.allott@gmail.com>
2018-01-21 13:13:45 +0100

    Commit env var values

--- src/env.js
+++ src/env.js
@@ -1,8 +1,10 @@
 // env var. Example to set it, execute in command line: REACT_APP_TRAVEL_BID=http://alberto.localhost.run yarn start
 const keyValue = process.env.REACT_APP_HOTELS_COMBINED_KEY_VALUE;
-export const HOTELS_COMBINED_KEY_VALUE = keyValue || '';
+// export const HOTELS_COMBINED_KEY_VALUE = keyValue || '';
+export const HOTELS_COMBINED_KEY_VALUE = '5FAB425F-56F4-4E7B-AF6D-9868557BDE31';
 
-const url = process.env.REACT_APP_TRAVEL_BID || '';
+// const url = process.env.REACT_APP_TRAVEL_BID || '';
+const url = 'http://backend:8080';
 export const HOTELS_COMBINED_BASE_URL = url + '/hotelscombined-sandbox';
 export const HOTELS_COMBINED_BASE_URL_PUBLIC = url + '/hotelscombined';
 export const TRAVEL_BID_GRAPHQL_URL = url + '/graphql';
